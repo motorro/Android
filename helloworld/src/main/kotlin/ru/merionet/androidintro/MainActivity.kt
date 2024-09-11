5592752f28e3e29ffba816ca41342d8bc515efb6
Nikolai Kotchetkov <motorro@gmail.com>
2024-09-11 18:24:38 +0200

    Hello World application Refs: #1

---
+++ helloworld/src/main/kotlin/ru/merionet/androidintro/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.merionet.androidintro
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.merionet.androidintro.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.hello.text = getString(R.string.hello_world)
+    }
+}
