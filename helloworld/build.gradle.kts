780079cd782b2866efcd3885b6c4afcb60318e73
Nikolai Kotchetkov <motorro@gmail.com>
2024-09-12 12:31:06 +0200

    Sample task Refs: #1
    (cherry picked from commit f5ff79612eae0b99242dc90a059c78e4efd9b11c)
    (cherry picked from commit b0cd577bc194f8217afde35ecdd5d3035fb164c4)

--- helloworld/build.gradle.kts
+++ helloworld/build.gradle.kts
@@ -49,3 +49,10 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
+
+tasks.register("hello") {
+    group = "samples"
+    doLast {
+        println("Hello, World!")
+    }
+}
