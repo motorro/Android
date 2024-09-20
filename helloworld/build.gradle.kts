30feda042e36b5cfb9d0d7fd1eb1315170373f74
Nikolai Kotchetkov <motorro@gmail.com>
2024-09-20 10:48:59 +0200

    Sample test Refs: #1

--- helloworld/build.gradle.kts
+++ helloworld/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
+    testImplementation(libs.kotlin.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
---
+++ helloworld/src/test/kotlin/com/motorro/androidintro/SampleTest.kt
@@ -0,0 +1,14 @@
+package com.motorro.androidintro
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class SampleTest {
+    @Test
+    fun twoPlusTwoEqualsFour() {
+        assertEquals(
+            4,
+            2 + 2
+        )
+    }
+}
