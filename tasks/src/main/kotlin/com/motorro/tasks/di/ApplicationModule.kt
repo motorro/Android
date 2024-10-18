1b26113cd9666b1573183efffd3b84e5e8b284d3
Nikolai Kotchetkov <motorro@gmail.com>
2024-10-18 18:03:10 +0200

    Tasks API Refs: #4
    (cherry picked from commit 2e4db3543965b817081ea601f176a2959b3bbe55)

--- tasks/src/main/kotlin/com/motorro/tasks/di/ApplicationModule.kt
+++ tasks/src/main/kotlin/com/motorro/tasks/di/ApplicationModule.kt
@@ -1,37 +1,20 @@
 package com.motorro.tasks.di
 
 import com.motorro.core.coroutines.DispatcherProvider
-import com.motorro.tasks.auth.SessionManager
-import com.motorro.tasks.data.httpResponseModule
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import io.ktor.client.HttpClient
-import io.ktor.client.plugins.auth.Auth
-import io.ktor.client.plugins.auth.providers.BearerTokens
-import io.ktor.client.plugins.auth.providers.bearer
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.serialization.json.Json
 import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Retention(AnnotationRetention.RUNTIME)
 @Qualifier
 annotation class App
-
-@Retention(AnnotationRetention.RUNTIME)
-@Qualifier
-annotation class LoginHttp
-
-@Retention(AnnotationRetention.RUNTIME)
-@Qualifier
-annotation class AppHttp
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -50,34 +33,4 @@ class ApplicationModule {
     fun applicationScope(): CoroutineScope = CoroutineScope(
         Dispatchers.Default + SupervisorJob()
     )
-
-    @LoginHttp
-    @Provides
-    @Singleton
-    fun loginHttpClient(): HttpClient = HttpClient {
-        install(ContentNegotiation) {
-            json(Json { serializersModule = httpResponseModule })
-        }
-    }
-
-    @AppHttp
-    @Provides
-    @Singleton
-    fun appHttpClient(sessionManager: SessionManager): HttpClient = HttpClient {
-        install(ContentNegotiation) {
-            json(Json { serializersModule = httpResponseModule })
-        }
-        install(Auth) {
-            // Bearer token does not expire for this demo
-            // But you could automatically refresh it here
-            // See: https://ktor.io/docs/client-bearer-auth.html
-            bearer {
-                loadTokens {
-                    sessionManager.getTokens().run {
-                        BearerTokens(access, refresh)
-                    }
-                }
-            }
-        }
-    }
 }
