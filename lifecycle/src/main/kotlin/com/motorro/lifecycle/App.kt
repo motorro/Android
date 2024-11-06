08b2ee2d86c20d06cda18d2a5a8d78639548c14a
Nikolai Kotchetkov <motorro@gmail.com>
2024-11-06 12:45:23 +0100

    Empty Activity Refs: #7

---
+++ lifecycle/src/main/kotlin/com/motorro/lifecycle/App.kt
@@ -0,0 +1,12 @@
+package com.motorro.lifecycle
+
+import android.app.Application
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Napier.base(DebugAntilog())
+    }
+}
