575d4dc3ab4cf5f196ea3f3271aad48b253b0ced
Nikolai Kotchetkov <motorro@gmail.com>
2024-11-06 12:31:50 +0100

    Common Kotlin options for all projects
    (cherry picked from commit 2848d639f861acea47b913fdd9ee2a224c10a572)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -9,4 +11,15 @@ plugins {
     alias(libs.plugins.ktor) apply false
     alias(libs.plugins.google.ksp) apply false
     alias(libs.plugins.hilt) apply false
+}
+
+allprojects {
+    tasks.withType<KotlinCompile>().configureEach {
+        compilerOptions {
+            freeCompilerArgs.addAll(listOf(
+                "-opt-in=kotlin.RequiresOptIn",
+                "-Xcontext-receivers"
+            ))
+        }
+    }
 }
