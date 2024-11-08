f6d170cc7cd3a20cc45c394b604c1193b4b796d4
Nikolai Kotchetkov <motorro@gmail.com>
2024-11-08 12:00:08 +0100

    Logging app start Refs: #7

--- lifecycle/src/main/kotlin/com/motorro/lifecycle/App.kt
+++ lifecycle/src/main/kotlin/com/motorro/lifecycle/App.kt
@@ -1,12 +1,14 @@
 package com.motorro.lifecycle
 
 import android.app.Application
+import com.motorro.core.log.Logging
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 
-class App : Application() {
+class App : Application(), Logging {
     override fun onCreate() {
         super.onCreate()
         Napier.base(DebugAntilog())
+        i { "Application onCreate" }
     }
 }
