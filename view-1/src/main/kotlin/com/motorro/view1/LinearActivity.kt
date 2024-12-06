6fd17b5db8d67eeab60f669659aa4acb9113fa6b
Nikolai Kotchetkov <motorro@gmail.com>
2024-12-06 07:46:31 +0100

    Linear layout without weight Refs: #11

--- view-1/src/main/kotlin/com/motorro/view1/LinearActivity.kt
+++ view-1/src/main/kotlin/com/motorro/view1/LinearActivity.kt
@@ -1,47 +1,28 @@
 package com.motorro.view1
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.motorro.view1.databinding.ActivityMainBinding
+import com.motorro.view1.databinding.ActivityLinearBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class LinearActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLinearBinding
 
     @SuppressLint("InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLinearBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        setupChange()
-        setupNavigation()
-    }
-
-    private fun setupChange()  {
-        binding.change.setOnClickListener {
-            binding.text.text = getString(R.string.changed_text)
-        }
-    }
-
-    private fun setupNavigation() {
-        binding.toFrame.setOnClickListener {
-            startActivity(Intent(this, FrameActivity::class.java))
-        }
-        binding.toLinear.setOnClickListener {
-            startActivity(Intent(this, LinearActivity::class.java))
         }
     }
 }
