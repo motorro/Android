21fd89ad11eee4db2a3320408e2bcfc9b9d66c24
Nikolai Kotchetkov <motorro@gmail.com>
2024-12-19 12:28:01 +0100

    Proprietary attribute API Refs: #13

--- view-2/src/main/kotlin/com/motorro/view2/MainActivity.kt
+++ view-2/src/main/kotlin/com/motorro/view2/MainActivity.kt
@@ -22,5 +22,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.like1.likes = 100500
     }
 }
