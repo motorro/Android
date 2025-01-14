135e1872761896dd8f3e672af82d2afe13d0f5d4
Nikolai Kotchetkov <motorro@gmail.com>
2025-01-14 11:43:11 +0100

    Generating notes Refs: #16

--- flow/src/main/kotlin/com/motorro/flow/data/constants.kt
+++ flow/src/main/kotlin/com/motorro/flow/data/constants.kt
@@ -4,3 +4,9 @@ package com.motorro.flow.data
  * Network delay
  */
 const val NETWORK_DELAY = 500L
+
+/**
+ * Minimum and maximum number of notes
+ */
+const val MIN_NOTES = 1
+const val MAX_NOTES = 10
--- flow/src/main/kotlin/com/motorro/flow/data/notes.kt
+++ flow/src/main/kotlin/com/motorro/flow/data/notes.kt
@@ -1,6 +1,7 @@
 package com.motorro.flow.data
 
 import kotlinx.coroutines.delay
+import kotlin.random.Random
 
 data class Note(
     val id: Int,
@@ -17,21 +18,18 @@ suspend fun getNotesForUser(userId: Int, tags: Set<Int>): Result<List<Note>> {
     return Result.success(notes[userId]?.filter { notes -> notes.tags.any { tags.contains(it) } } ?: emptyList())
 }
 
-internal val notes = mapOf(
-    users[0].id to listOf(
-        Note(1, users[0].id, tags[users[0].id]?.get(0)?.id?.let(::setOf).orEmpty(), "Note1"),
-        Note(2, users[0].id, tags[users[0].id]?.get(1)?.id?.let(::setOf).orEmpty(), "Note2")
-    ),
-    users[1].id to listOf(
-        Note(3, users[1].id, tags[users[1].id]?.get(0)?.id?.let(::setOf).orEmpty(), "Note3"),
-        Note(4, users[1].id, tags[users[1].id]?.get(1)?.id?.let(::setOf).orEmpty(), "Note4")
-    ),
-    users[2].id to listOf(
-        Note(5, users[2].id, tags[users[2].id]?.get(0)?.id?.let(::setOf).orEmpty(), "Note5"),
-        Note(6, users[2].id, tags[users[2].id]?.get(1)?.id?.let(::setOf).orEmpty(), "Note6")
-    ),
-    users[3].id to listOf(
-        Note(7, users[3].id, tags[users[3].id]?.get(0)?.id?.let(::setOf).orEmpty(), "Note7"),
-        Note(8, users[3].id, tags[users[3].id]?.get(1)?.id?.let(::setOf).orEmpty(), "Note8")
-    )
-)
+internal val notes: Map<Int, List<Note>> = users.associate {
+    it.id to createNotesForUser(it.id)
+}
+
+private fun createNotesForUser(userId: Int): List<Note> {
+    val tagsForUser = tags[userId] ?: emptyList()
+    return (1 .. Random.nextInt(MIN_NOTES, MAX_NOTES)).map {
+        Note(
+            it,
+            userId,
+            (1..tagsForUser.lastIndex).map { tagsForUser.random().id }.toSet(),
+            "Note$it"
+        )
+    }
+}
