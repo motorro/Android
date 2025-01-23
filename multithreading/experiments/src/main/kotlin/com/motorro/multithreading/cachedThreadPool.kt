310421ddb605048c39d3c342accb16300fd9903d
Nikolai Kotchetkov <motorro@gmail.com>
2025-01-23 09:28:20 +0100

    Cached Tread Pool Refs: #17

---
+++ multithreading/experiments/src/main/kotlin/com/motorro/multithreading/cachedThreadPool.kt
@@ -0,0 +1,13 @@
+package com.motorro.multithreading
+
+import java.util.concurrent.Executors
+
+fun main() {
+    val executor = Executors.newCachedThreadPool()
+    repeat(10) {
+        executor.execute {
+            log { "Running a task..." }
+        }
+    }
+    executor.shutdown()
+}
