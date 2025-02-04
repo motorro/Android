e005841ef01296ba3cb552442a5ceee95b32192c
Nikolai Kotchetkov <motorro@gmail.com>
2025-02-04 14:56:55 +0100

    Timer task Refs: #19

---
+++ practice/coroutines/src/main/kotlin/com/motorro/coroutines/ui/network/NetworkFragment.kt
@@ -0,0 +1,41 @@
+package com.motorro.coroutines.ui.network
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.motorro.coroutines.databinding.FragmentNetworkBinding
+
+class NetworkFragment : Fragment() {
+
+    private var _binding: FragmentNetworkBinding? = null
+    private val binding get() = _binding!!
+
+    private val networkViewModel by viewModels<NetworkViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNetworkBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val textView: TextView = binding.textNotifications
+        networkViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
