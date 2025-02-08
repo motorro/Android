4400369e3079790b44280a48477204881b73d1bd
Nikolai Kotchetkov <motorro@gmail.com>
2025-02-08 12:40:18 +0100

    Page events Refs: #20

---
+++ stateclassic/src/main/kotlin/com/motorro/stateclassic/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.motorro.stateclassic
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.motorro.stateclassic.databinding.ActivityMainBinding
+import com.motorro.stateclassic.stat.createPageEvent
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        (application as App).statService.logEvent(createPageEvent("onCreate"))
+    }
+
+    override fun onStart() {
+        super.onStart()
+        (application as App).statService.logEvent(createPageEvent("onStart"))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (application as App).statService.logEvent(createPageEvent("onResume"))
+    }
+
+    override fun onPause() {
+        super.onPause()
+        (application as App).statService.logEvent(createPageEvent("onPause"))
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (application as App).statService.logEvent(createPageEvent("onStop"))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        (application as App).statService.logEvent(createPageEvent("onDestroy"))
+    }
+}
---
+++ stateclassic/src/main/kotlin/com/motorro/stateclassic/stat/PageEvent.kt
@@ -0,0 +1,31 @@
+package com.motorro.stateclassic.stat
+
+import kotlin.time.Clock
+import kotlin.time.Instant
+
+
+data class PageEvent(
+    private val page: String,
+    private val action: String,
+    private val time: Instant
+) : Event {
+    override val name: String get() = PAGE
+
+    override val properties: Map<String, String> get() = mapOf(
+        NAME to page,
+        TIME to time.toString(),
+        ACTION to action
+    )
+
+    companion object {
+        private const val PAGE = "page"
+        private const val NAME = "name"
+        private const val TIME = "time"
+        private const val ACTION = "action"
+    }
+}
+
+fun <T: Any> T.createPageEvent(
+    action: String,
+    time: Instant = Clock.System.now()
+) = PageEvent(requireNotNull(this::class.simpleName), action, time)
