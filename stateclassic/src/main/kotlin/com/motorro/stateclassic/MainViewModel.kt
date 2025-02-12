0b567559d6aa06d5eb1974b77b93633d1f7a9c9c
Nikolai Kotchetkov <motorro@gmail.com>
2025-02-12 13:30:28 +0100

    Saving state to state handle Refs: #20

--- stateclassic/src/main/kotlin/com/motorro/stateclassic/MainViewModel.kt
+++ stateclassic/src/main/kotlin/com/motorro/stateclassic/MainViewModel.kt
@@ -1,19 +1,21 @@
 package com.motorro.stateclassic
 
 import android.graphics.Rect
+import androidx.annotation.VisibleForTesting
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.distinctUntilChanged
 import com.google.mlkit.vision.text.Text
 import com.motorro.stateclassic.prefs.KeyValueStorage
 
-class MainViewModel(private val storage: KeyValueStorage) : ViewModel() {
+class MainViewModel(private val storage: KeyValueStorage, private val savedStateHandle: SavedStateHandle) : ViewModel() {
     private var detectedText: String = ""
 
-    private val _content: MutableLiveData<String> = MutableLiveData("")
+    private val _content: MutableLiveData<String> = savedStateHandle.getLiveData(CONTENT_KEY, "")
     val content: LiveData<String> = MediatorLiveData("").apply {
         addSource(_content) { contentUpdate ->
             value = mergeContent(storage[PREFIX_KEY], contentUpdate)
@@ -29,8 +31,9 @@ class MainViewModel(private val storage: KeyValueStorage) : ViewModel() {
         return contentPrefix + contentValue
     }
 
-    private val _shareEnabled: MutableLiveData<Boolean> = MutableLiveData(false)
-    val shareEnabled: LiveData<Boolean> get() = _shareEnabled
+    val shareEnabled: LiveData<Boolean> = MediatorLiveData(false).apply {
+        addSource(content) { value = it.isNotBlank() }
+    }
 
     private val _addEnabled: MutableLiveData<Boolean> = MutableLiveData(false)
     val addEnabled: LiveData<Boolean> get() = _addEnabled
@@ -61,15 +64,20 @@ class MainViewModel(private val storage: KeyValueStorage) : ViewModel() {
 
     fun addDetectedText() {
         val text = detectedText.takeIf { it.isNotBlank() } ?: return
-        _content.postValue(_content.value + text)
-        _shareEnabled.postValue(true)
+        val soFar: String = savedStateHandle[CONTENT_KEY] ?: ""
+        savedStateHandle[CONTENT_KEY] = soFar + text
     }
 
-    class Factory(private val storage: KeyValueStorage) : ViewModelProvider.Factory {
-        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+    companion object {
+        @VisibleForTesting
+        const val CONTENT_KEY = "content"
+    }
+
+    class Factory(private val storage: KeyValueStorage) : AbstractSavedStateViewModelFactory() {
+        override fun <T : ViewModel> create(key: String, modelClass: Class<T>, handle: SavedStateHandle): T {
             if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
                 @Suppress("UNCHECKED_CAST")
-                return MainViewModel(storage) as T
+                return MainViewModel(storage, handle) as T
             }
             throw IllegalArgumentException("Unknown ViewModel class")
         }
