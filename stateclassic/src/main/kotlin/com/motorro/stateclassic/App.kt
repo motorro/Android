b697149e1c3e9537bc1bfd633d7cc01b6ea50dfc
Nikolai Kotchetkov <motorro@gmail.com>
2025-02-12 12:13:22 +0100

    Key-value storage Refs: #20

--- stateclassic/src/main/kotlin/com/motorro/stateclassic/App.kt
+++ stateclassic/src/main/kotlin/com/motorro/stateclassic/App.kt
@@ -1,6 +1,8 @@
 package com.motorro.stateclassic
 
 import android.app.Application
+import com.motorro.stateclassic.prefs.KeyValueStorage
+import com.motorro.stateclassic.prefs.SharedPreferencesStorage
 import com.motorro.stateclassic.stat.StatService
 
 class App : Application() {
@@ -8,4 +10,11 @@ class App : Application() {
      * Stat service
      */
     val statService = StatService.Logger()
+
+    /**
+     * Preferences storage
+     */
+    val preferences: KeyValueStorage by lazy {
+        SharedPreferencesStorage(this, "prefs")
+    }
 }
