431f00f135b6304ed12dbaf6ac4c95e6be678ac4
Nikolai Kotchetkov <motorro@gmail.com>
2025-02-27 11:13:06 +0100

    Navigation in tabs Refs: #22

--- navigation/src/main/kotlin/com/motorro/navigation/ui/TabsFragment.kt
+++ navigation/src/main/kotlin/com/motorro/navigation/ui/TabsFragment.kt
@@ -1,36 +1,21 @@
 package com.motorro.navigation.ui
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
-import androidx.fragment.app.commit
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.motorro.navigation.R
 import com.motorro.navigation.data.sessionManager
 import com.motorro.navigation.databinding.FragmentTabsBinding
-import com.motorro.navigation.ui.dashboard.DashboardFragment
-import com.motorro.navigation.ui.home.HomeFragment
-import com.motorro.navigation.ui.notifications.NotificationsFragment
 
 class TabsFragment : Fragment() {
     private var _binding: FragmentTabsBinding? = null
     private val binding get() = requireNotNull(_binding)
-
-    private lateinit var homeFragment: Fragment
-    private lateinit var dashboard: Fragment
-    private lateinit var notifications: Fragment
-
-    private val fragments: List<Fragment> get() = listOf(
-        homeFragment,
-        dashboard,
-        notifications
-    )
-
-    private var selectedIndex = 0
 
     private val session by sessionManager()
 
@@ -48,15 +33,9 @@ class TabsFragment : Fragment() {
         return binding.root
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setupFragments(savedInstanceState)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupNavigation()
-        binding.topAppBar.title = binding.navView.menu.getItem(selectedIndex).title
         binding.topAppBar.setOnMenuItemClickListener {
             when(it.itemId) {
                 R.id.menu_logout -> {
@@ -68,79 +47,22 @@ class TabsFragment : Fragment() {
         }
     }
 
-    @SuppressLint("CommitTransaction")
-    private fun setupFragments(savedInstanceState: Bundle?) {
-        if (null == savedInstanceState) {
-            homeFragment = HomeFragment.newInstance()
-            dashboard = DashboardFragment.newInstance()
-            notifications = NotificationsFragment.newInstance()
-
-            childFragmentManager.commit {
-                add(R.id.tabs, homeFragment, HomeFragment::class.java.name)
-                add(R.id.tabs, dashboard, DashboardFragment::class.java.name)
-                add(R.id.tabs, notifications, NotificationsFragment::class.java.name)
-                selectFragment(selectedIndex)
-            }
-        } else {
-            homeFragment = requireNotNull(childFragmentManager.findFragmentByTag(HomeFragment::class.java.name))
-            dashboard = requireNotNull(childFragmentManager.findFragmentByTag(DashboardFragment::class.java.name))
-            notifications = requireNotNull(childFragmentManager.findFragmentByTag(NotificationsFragment::class.java.name))
-        }
-    }
-
     private fun setupNavigation() = with(binding) {
-        navView.setOnItemSelectedListener { item ->
-            topAppBar.title = item.title
-            when(item.itemId) {
-                R.id.navigation_home -> {
-                    selectFragment(0)
-                    true
-                }
-                R.id.navigation_dashboard -> {
-                    selectFragment(1)
-                    true
-                }
-                R.id.navigation_notifications -> {
-                    selectFragment(2)
-                    true
-                }
-                else -> false
-            }
-        }
-    }
-
-    private fun FragmentTransaction.selectFragment(selectedIndex: Int): FragmentTransaction {
-        fragments.forEachIndexed { index, fragment ->
-            if (index == selectedIndex) {
-                attach(fragment)
-            } else {
-                detach(fragment)
-            }
-        }
-
-        setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-
-        return this
-    }
-
-    private fun selectFragment(indexToSelect: Int) {
-        this.selectedIndex = indexToSelect
-        childFragmentManager.beginTransaction()
-            .selectFragment(indexToSelect)
-            .commit()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt(SELECTED_INDEX, selectedIndex)
+        val navHostFragment = childFragmentManager.findFragmentById(R.id.tabs) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications
+            )
+        )
+        topAppBar.setupWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    companion object {
-        private const val SELECTED_INDEX = "selectedIndex"
     }
 }
