a27fafb5adfad6ba03616fe8e0506fd1fbc699c4
Nikolai Kotchetkov <motorro@gmail.com>
2025-03-09 22:29:08 +0100

    Task 4 solution Refs: #23

--- practice/navigation/src/main/kotlin/com/motorro/cookbook/ui/DeleteConfirmationFragment.kt
+++ practice/navigation/src/main/kotlin/com/motorro/cookbook/ui/DeleteConfirmationFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
+import com.motorro.cookbook.R
 import com.motorro.cookbook.databinding.FragmentConfirmationBinding
 
 class DeleteConfirmationFragment : DialogFragment() {
@@ -22,7 +24,7 @@ class DeleteConfirmationFragment : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.withBinding {
-            text.text = TODO("Get title from arguments")
+            text.text = getString(R.string.text_delete, DeleteConfirmationFragmentArgs.fromBundle(requireArguments()).title)
             ok.setOnClickListener {
                 setResult(true)
             }
@@ -33,6 +35,14 @@ class DeleteConfirmationFragment : DialogFragment() {
     }
 
     private fun setResult(result: Boolean) {
-        TODO("Set result and close")
+        findNavController().previousBackStackEntry?.savedStateHandle?.set(
+            CONFIRMATION_RESULT,
+            result
+        )
+        findNavController().navigateUp()
+    }
+
+    companion object {
+        const val CONFIRMATION_RESULT = "confirmation_result"
     }
 }
