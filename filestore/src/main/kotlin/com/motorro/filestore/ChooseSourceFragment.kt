985658a24531f74c86b651c02e0107518781b3ba
Nikolai Kotchetkov <motorro@gmail.com>
2025-03-12 18:31:07 +0100

    Getting picture with preview Refs: #24

--- filestore/src/main/kotlin/com/motorro/filestore/ChooseSourceFragment.kt
+++ filestore/src/main/kotlin/com/motorro/filestore/ChooseSourceFragment.kt
@@ -5,13 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.motorro.core.viewbinding.BindingHost
 import com.motorro.core.viewbinding.WithViewBinding
 import com.motorro.core.viewbinding.bindView
+import com.motorro.core.viewbinding.withBinding
 import com.motorro.filestore.databinding.FragmentChooseSourceBinding
 
 class ChooseSourceFragment : Fragment(), WithViewBinding<FragmentChooseSourceBinding> by BindingHost() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return bindView(container, FragmentChooseSourceBinding::inflate)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        withBinding {
+            btnShootSmall.setOnClickListener {
+                findNavController().navigate(ChooseSourceFragmentDirections.chooseToTakePreview())
+            }
+        }
     }
 }
