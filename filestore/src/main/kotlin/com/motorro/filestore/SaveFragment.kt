9ffde59fbbb2c53f589f750e0e6ec5522a4cbcec
Nikolai Kotchetkov <motorro@gmail.com>
2025-03-14 11:49:32 +0100

    Saving to media-store Refs: #24

--- filestore/src/main/kotlin/com/motorro/filestore/SaveFragment.kt
+++ filestore/src/main/kotlin/com/motorro/filestore/SaveFragment.kt
@@ -1,18 +1,27 @@
 package com.motorro.filestore
 
+import android.content.ContentValues
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.MimeTypeMap
 import androidx.core.content.FileProvider
 import androidx.core.net.toFile
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.snackbar.Snackbar
 import com.motorro.core.viewbinding.BindingHost
 import com.motorro.core.viewbinding.WithViewBinding
 import com.motorro.core.viewbinding.bindView
 import com.motorro.core.viewbinding.withBinding
 import com.motorro.filestore.databinding.FragmentSaveBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SaveFragment : Fragment(), WithViewBinding<FragmentSaveBinding> by BindingHost() {
     private val image: ImageWithComment get() = SaveFragmentArgs.fromBundle(requireArguments()).toSave
@@ -26,6 +35,17 @@ class SaveFragment : Fragment(), WithViewBinding<FragmentSaveBinding> by Binding
         withBinding {
             btnShare.setOnClickListener {
                 share()
+            }
+            btnMedia.setOnClickListener {
+                lifecycleScope.launch {
+                    saveToMediaStore()
+                    Snackbar.make(
+                        requireActivity(),
+                        it,
+                        getString(R.string.snack_saved_to_media, image.file.toFile().name),
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                }
             }
         }
     }
@@ -46,5 +66,40 @@ class SaveFragment : Fragment(), WithViewBinding<FragmentSaveBinding> by Binding
         }
 
         startActivity(Intent.createChooser(intent, getString(R.string.title_image_with_exif_comment)))
+    }
+
+    private suspend fun saveToMediaStore() = withContext(Dispatchers.IO) {
+        val resolver = requireContext().contentResolver
+        val imageCollection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        } else {
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+
+        val fileName = image.file.toFile().name
+        val fileMimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(
+            fileName.substringAfterLast('.')
+        )
+
+        val imageDetails = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+            put(MediaStore.Images.Media.DESCRIPTION, image.comment)
+            put(MediaStore.Images.Media.MIME_TYPE, fileMimeType)
+            put(MediaStore.Images.Media.IS_PENDING, 1)
+        }
+
+        val imageContentUri = checkNotNull(resolver.insert(imageCollection, imageDetails)) {
+            "Failed to create image content uri"
+        }
+
+        resolver.openOutputStream(imageContentUri).use { outputStream ->
+            image.file.toFile().inputStream().use { inputStream ->
+                inputStream.copyTo(outputStream!!)
+            }
+        }
+
+        imageDetails.clear()
+        imageDetails.put(MediaStore.Audio.Media.IS_PENDING, 0)
+        resolver.update(imageContentUri, imageDetails, null, null)
     }
 }
