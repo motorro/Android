4b77c89cebabe939747ee3f9acb2c64fec2deefa
Nikolai Kotchetkov <motorro@gmail.com>
2025-04-09 13:27:35 +0200

    Custom serializer for a file Refs: #26

--- network/data/src/test/kotlin/com/motorro/network/data/URISerializerTest.kt
+++ network/data/src/test/kotlin/com/motorro/network/data/URISerializerTest.kt
@@ -1,6 +1,9 @@
+@file:UseSerializers(URISerializer::class)
+
 package com.motorro.network.data
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.UseSerializers
 import kotlinx.serialization.json.Json
 import org.junit.Test
 import java.net.URI
@@ -8,29 +11,34 @@ import kotlin.test.assertEquals
 
 
 class URISerializerTest {
-    private val uri = URI("https://example.com")
+    private val uri1 = URI("https://example.com")
+    private val uri2 = URI("https://motorro.com")
 
     @Serializable
     data class UriContainer(
-        @Serializable(with = URISerializer::class)
-        val uri: URI
+        val uri1: URI,
+        val uri2: URI
     )
 
-    private val serializedContainer = """{"uri":"https://example.com"}"""
+    private val serializedContainer = """{"uri1":"https://example.com","uri2":"https://motorro.com"}"""
 
     @Test
     fun serializes() {
         assertEquals(
             serializedContainer,
-            Json.encodeToString(UriContainer.serializer(), UriContainer(uri))
+            Json.encodeToString(UriContainer.serializer(), UriContainer(uri1, uri2))
         )
     }
 
     @Test
     fun deserializes() {
         assertEquals(
-            uri,
-            Json.decodeFromString(UriContainer.serializer(), serializedContainer).uri
+            uri1,
+            Json.decodeFromString(UriContainer.serializer(), serializedContainer).uri1
+        )
+        assertEquals(
+            uri2,
+            Json.decodeFromString(UriContainer.serializer(), serializedContainer).uri2
         )
     }
 }
