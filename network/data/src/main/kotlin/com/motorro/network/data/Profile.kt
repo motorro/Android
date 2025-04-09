61cfc5a5a90da0f4be415872872930d0a6b6c1da
Nikolai Kotchetkov <motorro@gmail.com>
2025-04-09 13:19:05 +0200

    Custom serializer Refs: #26

--- network/data/src/main/kotlin/com/motorro/network/data/Profile.kt
+++ network/data/src/main/kotlin/com/motorro/network/data/Profile.kt
@@ -2,6 +2,7 @@ package com.motorro.network.data
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import java.net.URI
 import kotlin.time.Instant
 
 @Serializable
@@ -12,5 +13,7 @@ data class Profile(
     val age: Int,
     val phone: Phone,
     val registered: Instant,
+    @Serializable(with = URISerializer::class)
+    val userpic: URI,
     val interests: Set<String> = emptySet()
 )
