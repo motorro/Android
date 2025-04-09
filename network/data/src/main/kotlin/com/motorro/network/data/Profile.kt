96082e46281ac3fbba21945803f4d5d04d61be81
Nikolai Kotchetkov <motorro@gmail.com>
2025-04-09 11:55:11 +0200

    Custom name Refs: #26

--- network/data/src/main/kotlin/com/motorro/network/data/Profile.kt
+++ network/data/src/main/kotlin/com/motorro/network/data/Profile.kt
@@ -1,9 +1,11 @@
 package com.motorro.network.data
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Profile(
+    @SerialName("id")
     val userId: Int,
     val name: String,
     val age: Int,
