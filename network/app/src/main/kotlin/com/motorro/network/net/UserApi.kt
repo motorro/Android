3a27154ffb23aed346d1e6b74d5cfa128ecebf1f
Nikolai Kotchetkov <motorro@gmail.com>
2025-04-12 18:08:47 +0200

    Deleting user Refs: #26

--- network/app/src/main/kotlin/com/motorro/network/net/UserApi.kt
+++ network/app/src/main/kotlin/com/motorro/network/net/UserApi.kt
@@ -3,6 +3,7 @@ package com.motorro.network.net
 import com.motorro.network.data.Profile
 import com.motorro.network.data.User
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
@@ -28,4 +29,10 @@ interface UserApi {
      */
     @POST("users")
     suspend fun createUser(@Body profile: Profile): Result<User>
+
+    /**
+     * Returns a user profile
+     */
+    @DELETE("users/{id}")
+    suspend fun deleteUser(@Path("id") userId: Int)
 }
--- network/app/src/main/kotlin/com/motorro/network/net/usecase/GetUserList.kt
+++ network/app/src/main/kotlin/com/motorro/network/net/usecase/GetUserList.kt
@@ -3,6 +3,7 @@ package com.motorro.network.net.usecase
 import com.motorro.network.data.User
 import com.motorro.network.data.UserListItem
 import com.motorro.network.net.UserApi
+import com.motorro.network.session.SessionManager
 
 /**
  * Use case to get a list of users.
@@ -15,13 +16,16 @@ interface GetUserList {
      */
     suspend operator fun invoke(): Result<List<UserListItem>>
 
-    class Impl(private val userApi: UserApi) : GetUserList {
+    class Impl(private val userApi: UserApi, private val sessionManager: SessionManager) : GetUserList {
         override suspend fun invoke(): Result<List<UserListItem>> {
             return userApi.getUserList().mapCatching { mapToUserListItem(it) }
         }
 
-        private fun mapToUserListItem(users: List<User>): List<UserListItem> = users.map { user ->
-            UserListItem(user, false)
+        private fun mapToUserListItem(users: List<User>): List<UserListItem> {
+            val deleteEnabled = sessionManager.loggedIn.value
+            return users.map { user ->
+                UserListItem(user, deleteEnabled)
+            }
         }
     }
 }
