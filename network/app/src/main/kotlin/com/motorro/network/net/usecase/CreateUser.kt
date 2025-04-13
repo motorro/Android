74cdfee278326bb61503550c3b2d376fbb7612e1
Nikolai Kotchetkov <motorro@gmail.com>
2025-04-13 13:21:35 +0200

    Preparing for auth interceptor Refs: #26

--- network/app/src/main/kotlin/com/motorro/network/net/usecase/CreateUser.kt
+++ network/app/src/main/kotlin/com/motorro/network/net/usecase/CreateUser.kt
@@ -3,7 +3,6 @@ package com.motorro.network.net.usecase
 import com.motorro.network.data.Profile
 import com.motorro.network.data.User
 import com.motorro.network.net.UserApi
-import com.motorro.network.session.SessionManager
 
 /**
  * Create User use case
@@ -16,9 +15,9 @@ interface CreateUser {
      */
     suspend operator fun invoke(profile: Profile): Result<User>
 
-    class Impl(private val userApi: UserApi, private val sessionManager: SessionManager) : CreateUser {
+    class Impl(private val userApi: UserApi) : CreateUser {
         override suspend fun invoke(profile: Profile): Result<User> {
-            return userApi.createUser(profile, "Bearer ${sessionManager.token.value}")
+            return userApi.createUser(profile)
         }
     }
 }
