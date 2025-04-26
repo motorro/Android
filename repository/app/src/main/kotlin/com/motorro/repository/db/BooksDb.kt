af628e59e93e27bea3aa0845caf91a37c4d32956
Nikolai Kotchetkov <motorro@gmail.com>
2025-04-26 19:11:30 +0200

    Reading book with book repository Refs: #27

--- repository/app/src/main/kotlin/com/motorro/repository/db/BooksDb.kt
+++ repository/app/src/main/kotlin/com/motorro/repository/db/BooksDb.kt
@@ -1,19 +1,24 @@
 package com.motorro.repository.db
 
 import android.content.Context
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.motorro.repository.db.dao.BooksDao
+import com.motorro.repository.db.entity.BookInfoEntity
 import com.motorro.repository.db.entity.ListBookEntity
 
 /**
  * Local books data source
  */
 @Database(
-    entities = [ListBookEntity::class],
-    version = 1
+    entities = [ListBookEntity::class, BookInfoEntity::class],
+    version = 2,
+    autoMigrations = [
+        AutoMigration(1, 2)
+    ]
 )
 @TypeConverters(Converters::class)
 abstract class BooksDb : RoomDatabase() {
