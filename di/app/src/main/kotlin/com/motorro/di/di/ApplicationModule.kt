9ad830dc40f51d8e8e6d881df9074ce551958460
Nikolai Kotchetkov <motorro@gmail.com>
2025-06-22 14:26:06 +0200

    Auto-created builder Refs: #30

--- di/app/src/main/kotlin/com/motorro/di/di/ApplicationModule.kt
+++ di/app/src/main/kotlin/com/motorro/di/di/ApplicationModule.kt
@@ -1,5 +1,7 @@
 package com.motorro.di.di
 
+import android.content.Context
+import com.motorro.di.R
 import com.motorro.di.timer.Timer
 import com.motorro.di.timer.TimerImplementation
 import dagger.Module
@@ -10,13 +12,13 @@ import kotlinx.coroutines.CoroutineScope
  * Provides dependencies to container
  */
 @Module
-class ApplicationModule {
+class ApplicationModule(private val context: Context) {
     /**
      * Instructions on how to create timer
      */
     @Provides
     fun timer(scope: CoroutineScope): Timer = TimerImplementation(
-        title = "Application time",
+        title = context.getString(R.string.application_time),
         scope = scope,
         delayMillis = 100
     )
