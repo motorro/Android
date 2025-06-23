e43a2d2856205bc8734020ad0e7c06154cf40e96
Nikolai Kotchetkov <motorro@gmail.com>
2025-06-23 06:38:24 +0200

    Binding module Refs: #30

--- di/app/src/main/kotlin/com/motorro/di/di/ApplicationModule.kt
+++ di/app/src/main/kotlin/com/motorro/di/di/ApplicationModule.kt
@@ -2,13 +2,16 @@ package com.motorro.di.di
 
 import android.content.Context
 import com.motorro.di.R
+import com.motorro.di.timer.Timer
+import com.motorro.di.timer.TimerImplementation
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
 /**
  * Provides dependencies to container
  */
-@Module
+@Module(includes = [ApplicationBindingModule::class])
 class ApplicationModule {
     /**
      * App timer title
@@ -21,4 +24,18 @@ class ApplicationModule {
      */
     @Provides
     fun delayMillis(): Long = 100
+}
+
+/**
+ * Substitutes implementation to all requests for interface
+ */
+@Module
+interface ApplicationBindingModule {
+    /**
+     * Use [TimerImplementation] if [Timer] is required
+     * @param impl Implementation class
+     * @return [impl] as [Timer]
+     */
+    @Binds
+    fun timer(impl: TimerImplementation): Timer
 }
