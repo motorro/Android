028f6bbb2ead833a013f1fb7b402c20239adb4d7
Nikolai Kotchetkov <motorro@gmail.com>
2025-06-23 13:14:25 +0200

    Unscoped timers Refs: #30

--- di/app/src/main/kotlin/com/motorro/di/di/ApplicationModule.kt
+++ di/app/src/main/kotlin/com/motorro/di/di/ApplicationModule.kt
@@ -1,41 +1,49 @@
 package com.motorro.di.di
 
 import android.content.Context
+import android.util.Log
 import com.motorro.di.R
 import com.motorro.di.timer.Timer
 import com.motorro.di.timer.TimerImplementation
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
+import kotlinx.coroutines.CoroutineScope
+import kotlin.random.Random
 
 /**
  * Provides dependencies to container
  */
-@Module(includes = [ApplicationBindingModule::class])
+@Module
 class ApplicationModule {
-    /**
-     * App timer title
-     */
-    @Provides
-    fun title(context: Context): String = context.getString(R.string.application_time)
+    companion object {
+        /**
+         * Next timer number
+         */
+        private var nextTimerNumber = 1
+
+        /**
+         * Logging tag
+         */
+        private const val TAG = "ApplicationModule"
+    }
 
     /**
      * App timer delay
      */
     @Provides
-    fun delayMillis(): Long = 100
-}
+    fun delayMillis(): Long = Random.nextLong(50, 500)
 
-/**
- * Substitutes implementation to all requests for interface
- */
-@Module
-interface ApplicationBindingModule {
     /**
-     * Use [TimerImplementation] if [Timer] is required
-     * @param impl Implementation class
-     * @return [impl] as [Timer]
+     * Timer provider.
+     * Increments creation count each time the new timer is created
      */
-    @Binds
-    fun timer(impl: TimerImplementation): Timer
+    @Provides
+    fun timer(context: Context, delay: Long, scope: CoroutineScope): Timer {
+        Log.i(TAG, "Creating timer: $nextTimerNumber. Delay would be: $delay")
+        return TimerImplementation(
+            title = context.getString(R.string.application_time, nextTimerNumber++),
+            scope = scope,
+            delayMillis = delay
+        )
+    }
 }
