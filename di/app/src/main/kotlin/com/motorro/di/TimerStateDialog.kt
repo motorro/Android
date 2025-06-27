9ee3f7d428cdec4cb2734cfeeb1c08e8aab2ca25
Nikolai Kotchetkov <motorro@gmail.com>
2025-06-27 19:20:17 +0200

    Injecting saved state handle Refs: #30

---
+++ di/app/src/main/kotlin/com/motorro/di/TimerStateDialog.kt
@@ -0,0 +1,62 @@
+package com.motorro.di
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.motorro.core.viewbinding.BindingHost
+import com.motorro.core.viewbinding.WithViewBinding
+import com.motorro.core.viewbinding.bindView
+import com.motorro.core.viewbinding.withBinding
+import com.motorro.di.databinding.FragmentTimerStateBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
+import java.util.Locale
+import kotlin.time.Duration
+
+@AndroidEntryPoint
+class TimerStateDialog : DialogFragment(), WithViewBinding<FragmentTimerStateBinding> by BindingHost() {
+
+    private val viewModel: TimerStateViewModel by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return bindView(container, FragmentTimerStateBinding::inflate)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewLifecycleOwner.lifecycleScope.launch {
+            supervisorScope {
+                launch {
+                    viewModel.currentTime.collect {
+                        withBinding { timeSystem.text = it.toDisplayString() }
+                    }
+                }
+                launch {
+                    viewModel.providedTime.collect {
+                        withBinding { timeProvided.text = it.toDisplayString() }
+                    }
+                }
+            }
+        }
+        withBinding {
+            close.setOnClickListener {
+                dismiss()
+            }
+        }
+    }
+
+    private fun Duration.toDisplayString(): String {
+        val components = this.toComponents { minutes, seconds, nanos -> arrayOf(minutes.toInt(), seconds, nanos / 10_000_000) }
+
+        return String.format(
+            Locale.getDefault(),
+            "%02d:%02d.%02d",
+            *components
+        )
+    }
+}
---
+++ di/app/src/main/kotlin/com/motorro/di/TimerStateViewModel.kt
@@ -0,0 +1,32 @@
+package com.motorro.di
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.motorro.di.timer.Timer
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+import javax.inject.Named
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+
+@HiltViewModel
+class TimerStateViewModel @Inject constructor(
+    @Named("app") private val timer: Timer,
+    private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
+    /**
+     * Current time
+     */
+    val currentTime: StateFlow<Duration> get() = timer.count
+
+    /**
+     * Provided time
+     */
+    val providedTime: StateFlow<Duration> = MutableStateFlow((savedStateHandle.get<Long>(KEY_PROVIDED_TIME) ?: 0).milliseconds)
+
+    companion object {
+        const val KEY_PROVIDED_TIME = "providedTime"
+    }
+}
