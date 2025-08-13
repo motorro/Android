e4de1075307c0ed7df1ac6ecc984b128893c8322
Nikolai Kotchetkov <motorro@gmail.com>
2025-08-13 13:28:51 +0300

    Basic compose state Refs: #33

---
+++ composestate/compose/src/main/kotlin/com/motorro/composestate/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.motorro.composestate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.motorro.composestate.ui.Counter
+import com.motorro.composestate.ui.theme.AndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier.padding(innerPadding).fillMaxSize(),
+                        verticalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        Counter()
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ composestate/compose/src/main/kotlin/com/motorro/composestate/ui/Counter.kt
@@ -0,0 +1,55 @@
+package com.motorro.composestate.ui
+
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.motorro.composestate.R
+
+@Composable
+fun Counter() {
+    val count = remember { mutableIntStateOf(0) }
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Button(onClick = { count.intValue = (count.intValue - 1).coerceAtLeast(0) }) {
+            Text(text = stringResource(R.string.btn_decrement))
+        }
+
+        Text(
+            text = stringResource(R.string.lbl_count, count.intValue),
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Button(onClick = { count.intValue++ }) {
+            Text(text = stringResource(R.string.btn_increment))
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        snapshotFlow { count.intValue }.collect {
+            Log.i("Counter", "Count: $it")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CounterPreview() {
+    Counter()
+}
