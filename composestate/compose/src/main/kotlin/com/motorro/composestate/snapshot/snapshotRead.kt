095a3edbe4b974f599446d5f3bc123f61e035eb9
Nikolai Kotchetkov <motorro@gmail.com>
2025-08-19 12:54:46 +0300

    Snapshot reading Refs: #33

---
+++ composestate/compose/src/main/kotlin/com/motorro/composestate/snapshot/snapshotRead.kt
@@ -0,0 +1,25 @@
+package com.motorro.composestate.snapshot
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.snapshots.Snapshot
+
+fun main() {
+    val data: MutableState<String> = mutableStateOf("")
+    println("Initial state: ${data.value}")
+
+    data.value = "State 1"
+    println("Modification 1: ${data.value}")
+
+    // Take snapshot
+    val snapshot: Snapshot = Snapshot.takeSnapshot()
+
+    data.value = "State 2"
+    println("Modification 2: ${data.value}")
+
+    snapshot.enter {
+        println("Meanwhile in Snapshot: ${data.value}")
+    }
+
+    snapshot.dispose()
+}
