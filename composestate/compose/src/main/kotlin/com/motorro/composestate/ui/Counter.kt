61c0331da1e0ae7382a3c9f39d5046072f760820
Nikolai Kotchetkov <motorro@gmail.com>
2025-08-21 21:18:39 +0200

    Saveable counters Refs: #33

--- composestate/compose/src/main/kotlin/com/motorro/composestate/ui/Counter.kt
+++ composestate/compose/src/main/kotlin/com/motorro/composestate/ui/Counter.kt
@@ -6,21 +6,31 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.mapSaver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 // Non-savable class
-private class Counters {
-    var count1: Int by mutableIntStateOf(0)
-    var count2: Int by mutableIntStateOf(0)
+private class Counters(count1: Int = 0, count2: Int = 0) {
+    var count1: Int by mutableIntStateOf(count1)
+    var count2: Int by mutableIntStateOf(count2)
 }
 
 @Composable
 fun Counter() {
 
-    val count: Counters = remember { Counters() }
+    val counterSaver = run {
+        val count1Key = "count1"
+        val count2Key = "count2"
+        mapSaver(
+            save = { mapOf(count1Key to it.count1, count2Key to it.count2) },
+            restore = { Counters(it[count1Key] as Int, it[count2Key] as Int) }
+        )
+    }
+
+    val count: Counters = rememberSaveable(saver = counterSaver) { Counters() }
 
     Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceEvenly) {
         Counter(count = count.count1, onChange = { count.count1 = it })
