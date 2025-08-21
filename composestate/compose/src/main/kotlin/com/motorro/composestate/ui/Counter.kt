76159b357759c6a8eb0c0d21928e669bf9bfe39f
Nikolai Kotchetkov <motorro@gmail.com>
2025-08-21 19:22:06 +0200

    State delegate Refs: #33

--- composestate/compose/src/main/kotlin/com/motorro/composestate/ui/Counter.kt
+++ composestate/compose/src/main/kotlin/com/motorro/composestate/ui/Counter.kt
@@ -9,8 +9,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,29 +22,29 @@ import com.motorro.composestate.R
 
 @Composable
 fun Counter() {
-    val count = remember { mutableIntStateOf(0) }
+    var count: Int by remember { mutableIntStateOf(0) }
 
     Row(
         modifier = Modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
-        Button(onClick = { count.intValue = (count.intValue - 1).coerceAtLeast(0) }) {
+        Button(onClick = { count = (count - 1).coerceAtLeast(0) }) {
             Text(text = stringResource(R.string.btn_decrement))
         }
 
         Text(
-            text = stringResource(R.string.lbl_count, count.intValue),
+            text = stringResource(R.string.lbl_count, count),
             style = MaterialTheme.typography.headlineMedium
         )
 
-        Button(onClick = { count.intValue++ }) {
+        Button(onClick = { count++ }) {
             Text(text = stringResource(R.string.btn_increment))
         }
     }
 
     LaunchedEffect(Unit) {
-        snapshotFlow { count.intValue }.collect {
+        snapshotFlow { count }.collect {
             Log.i("Counter", "Count: $it")
         }
     }
