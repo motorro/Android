a1c23e8badc3dba5177e047ec93019251640dcb0
Nikolai Kotchetkov <motorro@gmail.com>
2025-09-21 17:53:30 +0200

    Common compose theme Refs: #35

--- cookbook/appcore/build.gradle.kts
+++ cookbook/appcore/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.compose)
 }
 
 android {
@@ -44,6 +45,13 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.material3)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
 
 kotlin {
