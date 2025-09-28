67aa149e67c6558d4b56ebba47a8b5d6503b1888
Nikolai Kotchetkov <motorro@gmail.com>
2025-09-28 11:16:40 +0200

    Passing arbitrary data between states Refs: #35

--- cookbook/recipe/recipedemo/src/main/kotlin/com/motorro/cookbook/recipedemo/state/RecipeProxy.kt
+++ cookbook/recipe/recipedemo/src/main/kotlin/com/motorro/cookbook/recipedemo/state/RecipeProxy.kt
@@ -1,6 +1,8 @@
 package com.motorro.cookbook.recipedemo.state
 
 import com.motorro.commonstatemachine.ProxyMachineState
+import com.motorro.cookbook.appcore.navigation.CommonFlowHost
+import com.motorro.cookbook.model.ListRecipe
 import com.motorro.cookbook.recipe.api.RecipeApi
 import com.motorro.cookbook.recipe.data.RecipeGesture
 import com.motorro.cookbook.recipe.data.RecipeViewState
@@ -22,11 +24,18 @@ private typealias RecipeProxyType = ProxyMachineState<
  */
 class RecipeProxy(
     private val api: RecipeApi,
-    private val recipeId: Uuid
+    private val recipeId: Uuid? = null,
+    private val listRecipe: ListRecipe? = null
 ) : RecipeProxyType(RecipeApi.DEFAULT_UI_STATE) {
 
-    override fun init() = api.start(recipeId) {
+    private val flowHost = CommonFlowHost {
         setMachineState(StarterState(api))
+    }
+
+    override fun init() = when {
+        recipeId != null -> api.start(recipeId, flowHost)
+        listRecipe != null -> api.start(listRecipe, flowHost)
+        else -> throw IllegalArgumentException("No recipeId or listRecipe provided")
     }
 
     override fun mapGesture(parent: RecipeDemoGesture): RecipeGesture? = when (parent) {
--- cookbook/recipe/src/main/kotlin/com/motorro/cookbook/recipe/api/RecipeApi.kt
+++ cookbook/recipe/src/main/kotlin/com/motorro/cookbook/recipe/api/RecipeApi.kt
@@ -2,6 +2,7 @@ package com.motorro.cookbook.recipe.api
 
 import com.motorro.commonstatemachine.CommonMachineState
 import com.motorro.cookbook.appcore.navigation.CommonFlowHost
+import com.motorro.cookbook.model.ListRecipe
 import com.motorro.cookbook.recipe.data.RecipeGesture
 import com.motorro.cookbook.recipe.data.RecipeViewState
 import kotlin.uuid.Uuid
@@ -17,6 +18,14 @@ interface RecipeApi {
      * @return Recipe flow state
      */
     fun start(recipeId: Uuid, flowHost: CommonFlowHost): CommonMachineState<RecipeGesture, RecipeViewState>
+
+    /**
+     * Starts add-recipe flow
+     * @param flowHost Flow host
+     * @param listRecipe Preloaded list recipe
+     * @return Recipe flow state
+     */
+    fun start(listRecipe: ListRecipe, flowHost: CommonFlowHost): CommonMachineState<RecipeGesture, RecipeViewState>
 
     companion object Companion {
         /**
