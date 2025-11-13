7bde9a2a9be9be16222a0693f078821e8a578a03
Nikolai Kotchetkov <motorro@gmail.com>
2025-11-13 14:18:54 +0100

    Library updates 2025-11

--- composecore/build.gradle.kts
+++ composecore/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material3.icons)
 
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
